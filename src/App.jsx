b2725da8eed1e4bd66f4dc0e92cbe66acdcc26bf
Shiyeon Ku <c.monk@wustl.edu>
2024-04-09 09:22:24 -0500

    made hello world
    created hello world application

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
     <>
@@ -17,18 +17,13 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <h1>Hello World</h1>
+        <p>This is my first React application</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
+    
   )
 }
 
